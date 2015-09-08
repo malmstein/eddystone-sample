b08796c09ff7f8c83ac01e56e3928b22dbcc1983
David Gonzalez <malmstein@gmail.com>
2015-09-08 19:06:34 +0100

    adding info views to activity

---
+++ app/src/main/java/com/malmstein/eddystonesample/manage/BeaconAttachmentsView.java
@@ -0,0 +1,21 @@
+package com.malmstein.eddystonesample.manage;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+public class BeaconAttachmentsView extends CardView {
+
+    public BeaconAttachmentsView(Context context) {
+        super(context);
+    }
+
+    public BeaconAttachmentsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BeaconAttachmentsView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
---
+++ app/src/main/java/com/malmstein/eddystonesample/manage/BeaconInfoView.java
@@ -0,0 +1,21 @@
+package com.malmstein.eddystonesample.manage;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+public class BeaconInfoView extends CardView {
+
+    public BeaconInfoView(Context context) {
+        super(context);
+    }
+
+    public BeaconInfoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BeaconInfoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
