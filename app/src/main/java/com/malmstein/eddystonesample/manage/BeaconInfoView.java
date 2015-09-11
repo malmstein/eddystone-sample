66e9e2f962bae0be042939252b09d1e7c237635e
David Gonzalez <malmstein@gmail.com>
2015-09-11 18:13:16 +0100

    allowing to change the coordinates of a beacon, still need to update the data upstream

--- app/src/main/java/com/malmstein/eddystonesample/manage/BeaconInfoView.java
+++ app/src/main/java/com/malmstein/eddystonesample/manage/BeaconInfoView.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.maps.model.LatLng;
 import com.malmstein.eddystonesample.R;
 import com.malmstein.eddystonesample.model.Beacon;
 import com.novoda.notils.caster.Views;
@@ -18,6 +22,9 @@ public class BeaconInfoView extends CardView {
     private TextView beaconPlace;
     private TextView beaconStability;
     private TextView beaconDescription;
+    private TextView beaconCoordinates;
+    private ImageView beaconMap;
+    private Listener listener;
 
     public BeaconInfoView(Context context) {
         super(context);
@@ -42,14 +49,55 @@ public class BeaconInfoView extends CardView {
         beaconPlace = Views.findById(this, R.id.beacon_info_place_id);
         beaconStability = Views.findById(this, R.id.beacon_info_stability);
         beaconDescription = Views.findById(this, R.id.beacon_info_description);
+        beaconCoordinates = Views.findById(this, R.id.beacon_info_coordinates);
+        beaconMap = Views.findById(this, R.id.beacon_info_map);
     }
 
-    public void updateWith(Beacon beacon) {
+    public void updateWith(Beacon beacon, Listener listener) {
+        this.listener = listener;
         beaconType.setText(beacon.getType());
         beaconId.setText(beacon.getHexId());
         beaconStatus.setText(beacon.getStatus().name());
         beaconPlace.setText(beacon.getPlaceId());
         beaconStability.setText(beacon.getExpectedStability());
         beaconDescription.setText(beacon.getDescription());
+
+        bindMap(beacon);
+    }
+
+    private void bindMap(final Beacon beacon) {
+        if (beacon.getLatLng() != null) {
+            updateStaticMapPicture(beacon.getLatLng());
+        }
+
+        beaconMap.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onRequestPlacePicker(beacon.getLatLng());
+            }
+        });
+    }
+
+    private void updateStaticMapPicture(LatLng latLng) {
+        String url = String.format(
+                "https://maps.googleapis.com/maps/api/staticmap?size=%1dx%2d&scale=2&markers=%.6f,%.6f",
+                beaconMap.getWidth(), beaconMap.getHeight(), latLng.latitude, latLng.longitude);
+        new FetchStaticMapTask(beaconMap).execute(url);
+    }
+
+    public void addPlace(Place place) {
+        beaconPlace.setText(place.getId());
+        beaconCoordinates.setText(place.getLatLng().toString());
+        updateStaticMapPicture(place.getLatLng());
+
+//        LatLng placeLatLng = place.getLatLng();
+//        bel.setText(placeLatLng.toString());
+//        beacon.latitude = placeLatLng.latitude;
+//        beacon.longitude = placeLatLng.longitude;
+//        updateBeacon();
+    }
+
+    public interface Listener {
+        void onRequestPlacePicker(LatLng latLng);
     }
 }
--- app/src/main/java/com/malmstein/eddystonesample/manage/ManageBeaconActivity.java
+++ app/src/main/java/com/malmstein/eddystonesample/manage/ManageBeaconActivity.java
@@ -1,19 +1,37 @@
 package com.malmstein.eddystonesample.manage;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.malmstein.eddystonesample.BuildConfig;
 import com.malmstein.eddystonesample.R;
+import com.malmstein.eddystonesample.account.AccountSharedPreferences;
 import com.malmstein.eddystonesample.model.Beacon;
+import com.malmstein.eddystonesample.proximitybeacon.ProximityBeaconImpl;
 import com.novoda.notils.caster.Views;
 
-public class ManageBeaconActivity extends AppCompatActivity {
+public class ManageBeaconActivity extends AppCompatActivity implements BeaconInfoView.Listener {
 
     public static final String KEY_BEACON = BuildConfig.APPLICATION_ID + "EXTRA_BEACON";
+
+    private static final int REQUEST_CODE_PLACE_PICKER = 1003;
+
     private BeaconInfoView beaconInfoView;
     private BeaconAttachmentsView beaconAttachmentsView;
+    private ProximityBeaconImpl proximityBeacon;
+
+    private Beacon getBeacon() {
+        return (Beacon) getIntent().getExtras().getSerializable(KEY_BEACON);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +42,16 @@ public class ManageBeaconActivity extends AppCompatActivity {
 
         beaconInfoView = Views.findById(this, R.id.beacon_info_view);
         beaconAttachmentsView = Views.findById(this, R.id.beacon_attachments_view);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PLACE_PICKER) {
+            if (resultCode == Activity.RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                beaconInfoView.addPlace(place);
+            }
+        }
     }
 
     private void setupToolbar() {
@@ -37,11 +65,32 @@ public class ManageBeaconActivity extends AppCompatActivity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        beaconInfoView.updateWith(getBeacon());
+        proximityBeacon = new ProximityBeaconImpl(this, AccountSharedPreferences.newInstance(this).getAccount());
+        beaconInfoView.updateWith(getBeacon(), this);
         beaconAttachmentsView.updateWith(getBeacon());
+        fetchNamespace();
     }
 
-    private Beacon getBeacon(){
-        return (Beacon) getIntent().getExtras().getSerializable(KEY_BEACON);
+    private void fetchNamespace() {
+
+    }
+
+    @Override
+    public void onRequestPlacePicker(LatLng latLng) {
+        try {
+            startPlacePickerIntnet(latLng);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startPlacePickerIntnet(LatLng latLng) throws GooglePlayServicesRepairableException, GooglePlayServicesNotAvailableException {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        if (latLng != null) {
+            builder.setLatLngBounds(new LatLngBounds(latLng, latLng));
+        }
+        startActivityForResult(builder.build(this), REQUEST_CODE_PLACE_PICKER);
     }
 }
